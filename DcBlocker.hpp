5461fa6825d356d7dd22901b618fa5197c5caa45
ezra buchla <emb@catfact.net>
2020-04-11 13:43:36 -0700

    test and fix dc blocker

--- DcBlocker.hpp
+++ DcBlocker.hpp
@@ -1,32 +1,30 @@
 // pole-zero highpass,
 // suitable for cheap DC blocking
 
-#include <cmath>
-
 namespace dspkit {
     template<typename T>
     class DcBlocker {
-	T c; // coefficient (pole position)
-	T x1; // input history
-	T y1; // output history
+        T c; // coefficient (pole position)
+        T x1; // input history
+        T y1; // output history
 
     public:
-	// initialize with sample rate and break frequency
-	void init(T sr, float fb=30) {
-	    static constexpr double pi = 3.1415926535898;
-	    double wn = pi * fb / sr;
-	    double b0 = 1.0 / (1.0 + wn);
-	    c = (1-wn) * b0;
-	    x1 = 0;
-	    y1 = 0;
-	}
+        // initialize with sample rate and break frequency
+        void init(T sr, float fb = 30) {
+            static constexpr double pi = 3.1415926535898;
+            double wn = pi * fb / sr;
+            double b0 = 1.0 / (1.0 + wn);
+            c = (1 - wn) * b0;
+            x1 = 0;
+            y1 = 0;
+        }
 
-	// process a single sample
-	T processSample(T x) {
-	    T y = x - x1 + c*y1;
-	    x1 = x;
-	    y1 = y;
-	    return y;
-	}
-    }
+        // process a single sample
+        T processSample(T x) {
+            T y = x - x1 + c * y1;
+            x1 = x;
+            y1 = y;
+            return y;
+        }
+    };
 }
